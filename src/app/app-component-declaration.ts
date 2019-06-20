458186e1a579dc5e046182fb2b1c3d52e592df65
DcParmar <dc.parmar27@yahoo.in>
2019-06-20 15:51:25 +0530

    Intigrate local project to server

--- src/app/app-component-declaration.ts
+++ src/app/app-component-declaration.ts
@@ -1,34 +1,34 @@
-import {AppComponent} from './app.component';
-import {LoginComponent} from './components/login/login.component';
-import {NavigationBarComponent} from './common-components/navigation-bar/navigation-bar.component';
-import {AuthService} from "./services/auth.service";
-import {LoginService} from "./services/login.service";
-import {LocalStorageService} from "./services/local-storage.service";
-import {HomePageComponent} from "./components/home-page/home-page.component";
-import {AuthGuardService} from "./services/auth-guard.service";
-import {MainLayoutComponent} from "./app-layouts/main-layout/main-layout.component";
-import {TimesheetViewComponent} from "./common-components/timesheet-view/timesheet-view.component";
-import {HandleHttpErrorsService} from "./services/handle-http-errors.service";
-import {TableRowElementComponent} from "./common-components/timesheet-view/timesheet-helpers/table-row-element.component";
-import {TableCellElementComponent} from "./common-components/timesheet-view/timesheet-helpers/table-cell-element.component";
-import {TableElementComponent} from "./common-components/timesheet-view/timesheet-helpers/timesheet-table-element.component";
-import {AppAutoCompleteComponent} from "./common-components/app-auto-complete/app-auto-complete.component";
-import {AutoCompleteService} from "./services/auto-complete.service";
-import {ProjectsComponent} from "./components/projects/projects.component";
-import {TasksComponent} from "./components/tasks/tasks.component";
-import {UsersComponent} from "./components/users/users.component";
-import {ConfirmModalDialogComponent} from "./common-components/modals/confirm-modal-dialog/confirm-modal-dialog.component";
-import {AllowOnlyNumbersDirective} from "./custom-directives/allow-only-numbers.directive";
-import {MyProfileComponent} from "./components/my-profile/my-profile.component";
-import {UserSettingsComponent} from "./components/user-settings/user-settings.component";
-import {PageNotFoundComponent} from "./components/page-not-found/page-not-found.component";
-import {NavigationBarHelperService} from "./services/navigation-bar-helper.service";
-import {CommonDataGridComponent} from "./common-components/common-data-grid/common-data-grid.component";
-import {SidebarComponent} from "./components/sidebar/sidebar.component";
-import {SidebarHelperService} from "./services/sidebar-helper.service";
-import {FilterArrayPipe} from "./services/filter-array.pipe";
-import {ProjectExpenseComponent} from "./components/project-expense/project-expense.component";
-
+import { AppComponent } from "./app.component";
+import { LoginComponent } from "./components/login/login.component";
+import { NavigationBarComponent } from "./common-components/navigation-bar/navigation-bar.component";
+import { AuthService } from "./services/auth.service";
+import { LoginService } from "./services/login.service";
+import { LocalStorageService } from "./services/local-storage.service";
+import { HomePageComponent } from "./components/home-page/home-page.component";
+import { AuthGuardService } from "./services/auth-guard.service";
+import { MainLayoutComponent } from "./app-layouts/main-layout/main-layout.component";
+import { TimesheetViewComponent } from "./common-components/timesheet-view/timesheet-view.component";
+import { HandleHttpErrorsService } from "./services/handle-http-errors.service";
+import { TableRowElementComponent } from "./common-components/timesheet-view/timesheet-helpers/table-row-element.component";
+import { TableCellElementComponent } from "./common-components/timesheet-view/timesheet-helpers/table-cell-element.component";
+import { TableElementComponent } from "./common-components/timesheet-view/timesheet-helpers/timesheet-table-element.component";
+import { AppAutoCompleteComponent } from "./common-components/app-auto-complete/app-auto-complete.component";
+import { AutoCompleteService } from "./services/auto-complete.service";
+import { ProjectsComponent } from "./components/projects/projects.component";
+import { TasksComponent } from "./components/tasks/tasks.component";
+import { UsersComponent } from "./components/users/users.component";
+import { ConfirmModalDialogComponent } from "./common-components/modals/confirm-modal-dialog/confirm-modal-dialog.component";
+import { AllowOnlyNumbersDirective } from "./custom-directives/allow-only-numbers.directive";
+import { MyProfileComponent } from "./components/my-profile/my-profile.component";
+import { UserSettingsComponent } from "./components/user-settings/user-settings.component";
+import { PageNotFoundComponent } from "./components/page-not-found/page-not-found.component";
+import { NavigationBarHelperService } from "./services/navigation-bar-helper.service";
+import { CommonDataGridComponent } from "./common-components/common-data-grid/common-data-grid.component";
+import { SidebarComponent } from "./components/sidebar/sidebar.component";
+import { SidebarHelperService } from "./services/sidebar-helper.service";
+import { FilterArrayPipe } from "./services/filter-array.pipe";
+import { ProjectExpenseComponent } from "./components/project-expense/project-expense.component";
+import { ProjectFormComponent } from "./components/project-form/project-form.component";
 
 export const appComponents = [
   MainLayoutComponent,
@@ -53,8 +53,8 @@ export const appComponents = [
   SidebarComponent,
   FilterArrayPipe,
   ProjectExpenseComponent,
+  ProjectFormComponent
 ];
-
 
 export const appServices = [
   AuthService,
@@ -65,9 +65,7 @@ export const appServices = [
   AutoCompleteService,
   AllowOnlyNumbersDirective,
   NavigationBarHelperService,
-  SidebarHelperService,
+  SidebarHelperService
 ];
 
-export const appEntryComponents = [
-  ConfirmModalDialogComponent
-];
+export const appEntryComponents = [ConfirmModalDialogComponent];
---
+++ src/app/components/project-form/project-form.component.ts
@@ -0,0 +1,21 @@
+import {Component, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-project-form',
+  templateUrl: './project-form.component.html',
+  styleUrls: ['./project-form.component.sass']
+})
+export class ProjectFormComponent implements OnInit {
+  selectedType: any;
+  projectTypes: any;
+
+  constructor() {
+  }
+
+  ngOnInit() {
+  }
+
+  onFormSubmit() {
+
+  }
+}
