e5a5d9abf2f9fdc055f18273d72a39dede898948
krupali viradiya <viradiyakrupali1@gmail.com>
2019-06-22 13:39:20 +0530

    merge error resolved

--- src/app/services/customer.service.ts
+++ src/app/services/customer.service.ts
@@ -25,11 +25,25 @@ export class CustomerService {
     });
   }
 
+  public getAllCustomers(): Promise<Array<CustomerModel>> {
+    return new Promise((resolve, reject) => {
+      this.appRestService.doGet(this.customerAPI).subscribe(
+        c => {
+          let _c = new Array<CustomerModel>();
+          Object.assign(_c, c);
+          resolve(_c);
+        },
+        err => {
+          reject(err);
+        }
+      );
+    });
+  }
   public createCustomer(model: CustomerModel, profilePicture) {
     return new Promise((onResolve, onReject) => {
       // const formData = new FormData();
       // formData.append("profilePicture", profilePicture, profilePicture.name);
-      // let modelKeys = Object.keys(model);
+      // let modelKeys = Object.keys(model    );
       // modelKeys.forEach(Key => {
       //   formData.append(Key, model[Key]);
       // });
