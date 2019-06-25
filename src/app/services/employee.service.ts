17a39e38c6a7b6c2e28d24494a9994ea10b03f08
vaishali vekariya <vaishalijpatel78@gmail.com>
2019-06-25 12:36:11 +0530

    Update employee.service.ts

--- src/app/services/employee.service.ts
+++ src/app/services/employee.service.ts
@@ -1,15 +1,41 @@
 import { Injectable } from "@angular/core";
 import { AppRestService } from "./app-rest.service";
+import { Employee } from "../models/employee.model";
 import { EmployeesModel } from "../models/employee";
 
 @Injectable({
   providedIn: "root"
 })
 export class EmployeeService {
+  private apiUrl: string = "api/employee/";
   private employeeAPIEndPoint: string = "/api/employee/";
 
-  constructor(private appRestService: AppRestService) {}
+  constructor(
+    private httpService: AppRestService,
+    private appRestService: AppRestService
+  ) {}
 
+  public getEmployeesForDropDown(): Promise<Array<Employee>> {
+    return new Promise((onRequestAccepted, onRequestRejected) => {
+      this.httpService.doGet(this.apiUrl).subscribe(
+        emps => {
+          let _emps = new Array<Employee>();
+          Object.assign(_emps, emps);
+          let defaulSelectCustomerC = new Employee();
+          defaulSelectCustomerC.FullName = "--Select--";
+          _emps = [...[defaulSelectCustomerC], ..._emps];
+          _emps.forEach(element => {
+            if (element.FirstName && element.LastName)
+              element.FullName = element.FirstName + " " + element.LastName;
+          });
+          onRequestAccepted(_emps);
+        },
+        error => {
+          onRequestRejected(null);
+        }
+      );
+    });
+  }
   public createEmployee(model: EmployeesModel) {
     return new Promise((onResolve, onReject) => {
       this.appRestService.doPost(this.employeeAPIEndPoint, model).subscribe(
