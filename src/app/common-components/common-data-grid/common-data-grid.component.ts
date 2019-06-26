99b837a0640b94f62a368d0712eb5d738562b811
krupali viradiya <viradiyakrupali1@gmail.com>
2019-06-26 15:13:47 +0530

    added tableActionColumn in Common data grid Component

--- src/app/common-components/common-data-grid/common-data-grid.component.ts
+++ src/app/common-components/common-data-grid/common-data-grid.component.ts
@@ -1,22 +1,32 @@
-import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
-import {TableColumnDefinition} from "../../models/table-column-definition";
+import { Component, EventEmitter, Input, OnInit, Output } from "@angular/core";
+import { TableColumnDefinition } from "../../models/table-column-definition";
 
 @Component({
-  selector: 'app-common-data-grid',
-  templateUrl: './common-data-grid.component.html',
-  styleUrls: ['./common-data-grid.component.sass']
+  selector: "app-common-data-grid",
+  templateUrl: "./common-data-grid.component.html",
+  styleUrls: ["./common-data-grid.component.sass"]
 })
 export class CommonDataGridComponent implements OnInit {
-  @Input('table-columns') tableHeaderColumns: Array<TableColumnDefinition> = [];
-  @Input('table-action-columns') tableActionColumns: Array<TableColumnDefinition> = [];
-  @Input('table-data') tableRenderingData: Array<any> = [];
+  @Input("table-columns") tableHeaderColumns: Array<TableColumnDefinition> = [];
+  @Input("table-action-columns") tableActionColumns: Array<
+    TableColumnDefinition
+  > = [];
+  @Input("table-data") tableRenderingData: Array<any> = [];
 
-  @Output('on-something-happens') onSomethingHappened: EventEmitter<any> = new EventEmitter();
+  @Output("on-event-performed") onEventPerformed: EventEmitter<
+    any
+  > = new EventEmitter();
 
-  constructor() {
+  constructor() {}
+
+  ngOnInit() {}
+
+  public OnActionButtonClicked(event, button, rowItem) {
+    console.info("OnActionButtonClicked", button);
+    this.onEventPerformed.emit({
+      event: event,
+      actionButton: button,
+      rowItem: rowItem
+    });
   }
-
-  ngOnInit() {
-  }
-
 }
